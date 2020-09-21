c1f7aecf5476d15835750feb6c8c4e6633628221
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-09-21 19:55:26 +0300

    converts html files to ejs

--- site/bulletin_board/routes/posts.js
+++ site/bulletin_board/routes/posts.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Bulletin Board' });
+router.get('/recent', function (req, res, next) {
+  res.render('post_list', { title: 'Recent Posts' });
 });
 
 module.exports = router;
